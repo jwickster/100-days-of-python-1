d5ead376264b83e40ebeb8dd8c2e16539065d775
Josh Wickster <joshwickster@gmail.com>
2020-11-08 09:51:54 -0700

    added case statements

--- Day1/index.py
+++ Day1/index.py
@@ -1,2 +1,10 @@
-print("Day 1 - Python print function")
-print("why should I print('what to print')")
+# print("Day 1 - Python print function")
+# print("why should I print('what to print')?")
+
+print("This will generate a band name based upon name of the city you grew up in")
+
+city = input("What is the name of the city you grew up in?\n")
+
+pet = input('What is the name of your pets name?\n')
+
+print("Your bands name is " + city + ' ' + pet)
