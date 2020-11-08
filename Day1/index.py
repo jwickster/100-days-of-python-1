01c371dc5d7f8ebdde894ed02ec9227bd7bde2b9
Josh Wickster <joshwickster@gmail.com>
2020-11-08 10:16:31 -0700

    added debugging exercise

--- Day1/index.py
+++ Day1/index.py
@@ -8,3 +8,10 @@ city = input("What is the name of the city you grew up in?\n")
 pet = input('What is the name of your pets name?\n')
 
 print("Your bands name is " + city + ' ' + pet)
+
+# Debugging Exercise
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the " + " sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
