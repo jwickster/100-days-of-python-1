868f5bb9cd05e5771a275d90b4e94f16cec84b15
Josh Wickster <joshwickster@gmail.com>
2020-11-09 08:19:47 -0700

    added exercises

--- Day1/index.py
+++ Day1/index.py
@@ -1,17 +1,26 @@
 # print("Day 1 - Python print function")
 # print("why should I print('what to print')?")
 
-print("This will generate a band name based upon name of the city you grew up in")
+# print("This will generate a band name based upon name of the city you grew up in")
+#
+# city = input("What is the name of the city you grew up in?\n")
+#
+# pet = input('What is the name of your pets name?\n')
+#
+# print("Your bands name is " + city + ' ' + pet)
+#
+# # Debugging Exercise
+#
+# print("Day 1 - String Manipulation")
+# print('String Concatenation is done with the " + " sign.')
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
 
-city = input("What is the name of the city you grew up in?\n")
 
-pet = input('What is the name of your pets name?\n')
+# input functions
 
-print("Your bands name is " + city + ' ' + pet)
+# firstName = input('Enter your first name')
+# print(len(firstName))
 
-# Debugging Exercise
+print(len(input("What is your name?")))
 
-print("Day 1 - String Manipulation")
-print('String Concatenation is done with the " + " sign.')
-print('e.g. print("Hello " + "world")')
-print("New lines can be created with a backslash and n.")
